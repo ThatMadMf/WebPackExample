a2dca985f71d094aa872c934659db9cd4d8b2ebb
ThatMadMf <maxdarksiders@gmail.com>
2020-11-13 20:16:43 +0200

    Add card component

--- client/src/app.js
+++ client/src/app.js
@@ -1,10 +1,22 @@
+import _ from 'lodash';
+import clock from 'clock';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.scss'
 
 import { renderFooter } from './components/footer/footer';
 
 import { renderHeader } from './components/header/header';
+import { renderMain } from './components/main/main';
+import { renderCard } from './components/card/card';
+
 
 const app = document.getElementById('app');
 
-app.innerHTML = renderHeader() + renderFooter();
+app.innerHTML = renderHeader() + renderMain() + renderFooter();
+
+const main = document.getElementById('main');
+
+for (let i = 0; i < 3; i++) {
+    main.innerHTML += renderCard();
+}
---
+++ client/src/components/card/card.js
@@ -0,0 +1,3 @@
+export const renderCard = () => {
+    return require('./card.html')
+}
